2c766dc66429424535e4ad873e1438019acaf190
Wilckerson <wilckerson@gmail.com>
2021-02-21 11:14:39 -0300

    Initial project files

---
+++ MicrotonalTuner/src/pages/Home.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import {View, Text} from 'react-native';
+const pages = () => {
+  return (
+    <View>
+      <Text>Home test</Text>
+    </View>
+  );
+};
+
+export default pages;
