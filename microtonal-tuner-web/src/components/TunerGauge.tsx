9f638583ab876050cf0287c19220f975fec0d930
Wilckerson <wilckerson@gmail.com>
2021-02-21 14:24:14 -0300

    Initial gauge component

--- microtonal-tuner-web/src/components/TunerGauge.tsx
+++ microtonal-tuner-web/src/components/TunerGauge.tsx
@@ -1,7 +1,41 @@
-import React from "react";
+import React, { useEffect, useRef } from "react";
+import GaugeObjects from "gaugeJS/dist/gauge";
 
 function TunerGauge() {
-  return <div>gauge</div>;
+  const gaugeRef = useRef<HTMLCanvasElement>(null);
+
+  useEffect(() => {
+    initGauge();
+  }, []);
+
+  function initGauge() {
+    var opts = {
+      angle: 0.15, /// The span of the gauge arc
+      lineWidth: 0.44, // The line thickness
+      pointer: {
+        length: 0.9, // Relative to gauge radius
+        strokeWidth: 0.035, // The thickness
+      },
+      colorStart: "#6FADCF", // Colors
+      colorStop: "#8FC0DA", // just experiment with them
+      strokeColor: "#E0E0E0", // to see which osnes work best for you
+    };
+    var gauge = new GaugeObjects.Gauge(gaugeRef.current).setOptions(opts);
+    gauge.maxValue = 3000; // set max gauge value
+    gauge.setMinValue(0); // Prefer setter over gauge.minValue = 0
+    gauge.animationSpeed = 32; // set animation speed (32 is default value)
+    gauge.set(1825); // set actual value
+  }
+
+  return (
+    <div>
+      <canvas
+        ref={gaugeRef}
+        //  width={300} height={300}
+        style={{ width: "100%" }}
+      ></canvas>
+    </div>
+  );
 }
 
 export default TunerGauge;
---
+++ microtonal-tuner-web/src/index.d.ts
@@ -0,0 +1,19 @@
+declare module "gaugeJS/dist/gauge" {
+  declare class Gauge {
+    maxValue: Number;
+    animationSpeed: Number;
+
+    constructor(element: HTMLCanvasElement | null);
+
+    setOptions(options: Object): Gauge;
+    setMinValue(value: Number);
+    set(value: Number);
+  }
+  const GaugeObjects = {
+    Gauge: Gauge,
+    Donut: any,
+    BaeDonut: any,
+    TextRenderer: any,
+  };
+  export default GaugeObjects;
+}
